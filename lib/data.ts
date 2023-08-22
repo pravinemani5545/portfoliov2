efe2a84585235d1225229369476b2dcceb8cd96e
pravinemani5545 <pravinemanivannan8@gmail.com>
2023-08-22 19:52:56 -0400

    Added Experiences, still need some adjustments

--- lib/data.ts
+++ lib/data.ts
@@ -37,28 +37,28 @@ export const links = [
 
 export const experiencesData = [
   {
-    title: 'Graduated bootcamp',
-    location: 'Miami, FL',
+    title: 'Computer Engineering Undergrad',
+    location: 'Hamilton, ON',
     description:
-      'I graduated after 6 months of studying. I immediately found a job as a front-end developer.',
+      'Started my long journey as a Computer Engineering undergrad. Currently in my 2nd last year of my program.',
     icon: React.createElement(LuGraduationCap),
     date: '2019',
   },
   {
-    title: 'Front-End Developer',
-    location: 'Orlando, FL',
+    title: '5G Software Developer Co-op',
+    location: 'Ottawa, ON',
     description:
-      'I worked as a front-end developer for 2 years in 1 job and 1 year in another job. I also upskilled to the full stack.',
+      'I worked on a 5G feature called Carrier Aggregation. My day to day included unit-testing and working on features. Worked primarily in Golang.',
     icon: React.createElement(CgWorkAlt),
-    date: '2019 - 2021',
+    date: '2022',
   },
   {
     title: 'Full-Stack Developer',
-    location: 'Houston, TX',
+    location: 'Toronto, ON',
     description:
-      "I'm now a full-stack developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind, Prisma and MongoDB. I'm open to full-time opportunities.",
+      "I'm now a full-stack developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind, Mongoose and MongoDB. I'm open to part-time and full-time opportunities.",
     icon: React.createElement(FaReact),
-    date: '2021 - present',
+    date: '2023 - present',
   },
 ] as const;
 
@@ -119,12 +119,12 @@ export const skillsData = [
   'Tailwind',
   'Prisma',
   'MongoDB',
+  'Mongoose',
   'Redux',
-  'GraphQL',
-  'Apollo',
   'Express',
-  'PostgreSQL',
   'Python',
-  'Django',
+  'Golang',
   'Framer Motion',
+  'Responsive Design',
+  'Figma',
 ] as const;
